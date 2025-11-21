e77d6c54f991ca491ca8275911d9486bd4f79f34
ruili <mary4net@gmail.com>
2025-11-21 12:29:01 +0800

    picture

--- load.py
+++ load.py
@@ -1,41 +1,28 @@
 from diffusers import DiffusionPipeline
 import torch
 
-def force_half(model):
-    for child in model.children():
-        try:
-            child.to(torch.float16)
-        except:
-            pass
-        force_half(child)
-
 # Load Stable Diffusion XL Base1.0
 pipe = DiffusionPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.float16,
+    dtype=torch.float16,
     variant="fp16",
-    use_safetensors=True,
-    device_map="balanced",
-    max_memory={0: "3GiB", "cpu": "16GiB"}
+    use_safetensors=True
 )
+pipe.to("cuda")
+
+# Optional CPU offloading to save some GPU Memory
+pipe.enable_model_cpu_offload()
 
 # Loading Trained DreamBooth LoRA Weights
-pipe.load_lora_weights("mary-ruiliii/genshin-style_character_generator")
-
-force_half(pipe.unet)
-force_half(pipe.vae)
-force_half(pipe.text_encoder)
-force_half(pipe.text_encoder_2)
-
-prompt = "栗黎尔"
+pipe.load_lora_weights("mary-ruiliii/genshin-style_character_generator", weight_dtype=torch.float16)
 
 # Invoke pipeline to generate image
-with torch.inference_mode():
-    image = pipe(
-        prompt = prompt,
-        num_inference_steps=50,
-        height=512,
-        width=512,
-        guidance_scale=7.0,
-    ).images[0]
-image.save(f"{prompt}.png")
+image = pipe(
+    prompt = "genshen-style character",
+    num_inference_steps=50,
+    height=1024,
+    width=1024,
+    # guidance_scale=7.0,
+).images[0]
+image.save("test.png")
+
